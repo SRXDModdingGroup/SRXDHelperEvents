71742482e37596e1ffdbd4f82b197d30fc52ebd6
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-01-07 15:53:18 -0800

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace SRXDHelperEvents {
+    [BepInPlugin("SRXD.HelperEvents", "HelperEvents", "1.0.0.0")]
+    public class Main : BaseUnityPlugin {
+        private void Awake() {
+            
+        }
+    }
+}
